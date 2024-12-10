78bd0082a9e3ea725102cd9ec59484fce727ae44
전증훈 <202480032@jei.com>
2024-12-10 15:37:10 +0900

    🔧 chore: tanstack query 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
     <div>
       <Suspense>
-        <Outlet />
+        <QueryClientProvider client={queryClient}>
+          <Outlet />
+        </QueryClientProvider>
       </Suspense>
     </div>
   );
