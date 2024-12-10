c0f1bef598288023f1cbf1977340dd008752ffc7
전증훈 <202480032@jei.com>
2024-12-10 15:05:21 +0900

    chore: emotion 추가

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,3 +1,18 @@
 export default function Main() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      {/* <S.Button>heelo</S.Button>
+      <Button>world</Button> */}
+    </div>
+  );
 }
+
+// const S = {
+//   Button: styled.button`
+//     color: hotpink;
+//   `,
+// };
+
+// const Button = styled.button`
+//   color: hotpink;
+// `;
