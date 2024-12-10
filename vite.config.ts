984818143bceb4124d95b0f1ca8e8dde6fcc9f91
전증훈 <202480032@jei.com>
2024-12-10 15:23:56 +0900

    🔧 chore: vitest react-testing-library 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import path from 'path';
 import { defineConfig } from 'vite';
 
@@ -6,9 +7,16 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./__tests__/setup.ts'],
   },
 });
