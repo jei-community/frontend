435bfb8331d786ce269024e1b2e8507a865172b1
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-12-11 15:20:27 +0900

    feat: `GlobalStyle` 구현 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router';
 
+import GlobalStyle from '@/styles/GlobalStyle';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <div>
-      <Suspense>
-        <QueryClientProvider client={queryClient}>
-          <Outlet />
-        </QueryClientProvider>
-      </Suspense>
-    </div>
+    <Suspense>
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        <Outlet />
+      </QueryClientProvider>
+    </Suspense>
   );
 }
