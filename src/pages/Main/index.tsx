e6c0e9dd16445a15d64f65e1f8a27d8a77515b2f
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-12-12 16:06:15 +0900

    👷 build: 배포 테스트8

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,29 +1,31 @@
+import styled from '@emotion/styled';
+
 export default function Main() {
   return (
     <div>
-      {/* <S.Button>heelo</S.Button>
-      <Button>world</Button> */}
+      <S.Button>heelo</S.Button>
+      <Button>world</Button>
     </div>
   );
 }
 
-// const S = {
-//   Button: styled.button`
-//     ${({ theme }) => `
-//     color: ${theme.colors.primary[500]};
-//     background-color: ${theme.colors.secondary[500]};
-//     font-family: ${theme.typography.title.fontFamily};
-//     font-size: ${theme.typography.title.fontSize};
-//     font-weight: ${theme.typography.title.fontWeight};
-//     line-height: ${theme.typography.title.lineHeight};
-//   `}
-//     padding: 10rem;
-//   `,
-// };
+const S = {
+  Button: styled.button`
+    ${({ theme }) => `
+    color: ${theme.colors.primary[500]};
+    background-color: ${theme.colors.secondary[500]};
+    font-family: ${theme.typography.title.fontFamily};
+    font-size: ${theme.typography.title.fontSize};
+    font-weight: ${theme.typography.title.fontWeight};
+    line-height: ${theme.typography.title.lineHeight};
+  `}
+    padding: 10rem;
+  `,
+};
 
-// const Button = styled.button`
-//   color: hotpink;
-// `;
+const Button = styled.button`
+  color: hotpink;
+`;
 
 // 객체형으로 Emotion 지정하는 예시:: 공식 홈페이지에서 추천하는 방법
 // const test = styled.div(({ theme }) => ({
