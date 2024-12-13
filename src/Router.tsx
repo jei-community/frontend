d35cbae5d4947c760ff3838af4eaf1a4596fcd99
jiaezzang <visveryver2@gmail.com>
2024-12-13 14:33:49 +0900

    ✨ feat : 로그인 페이지 라우터 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,14 +3,15 @@ import { Route, Routes } from 'react-router';
 import App from '@/App';
 import Main from '@/pages/Main';
 import NotFound from '@/pages/NotFound';
+import SignIn from '@/pages/SignIn';
 
 export default function Router() {
   return (
     <Routes>
+      <Route path='/sign-in' element={<SignIn />} />
       <Route path='/' element={<App />}>
         <Route path='/' element={<Main />} />
       </Route>
-
       <Route path='*' element={<NotFound />} />
     </Routes>
   );
