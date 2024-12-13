c5baef663c89fbed70fd5f657e1d396513162e68
임범규 <202380111@jei.com>
2024-12-13 14:24:53 +0900

    ♻️ refactor: 유틸함수 분리

--- src/components/DraggableScroll/index.tsx
+++ src/components/DraggableScroll/index.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren, useRef } from 'react';
 
-import { useDraggable } from './hooks/useDraggable';
+import useDraggable from './hooks/useDraggable';
 import { S } from './styles';
 
 export default function DraggableScroller({ children }: PropsWithChildren) {
