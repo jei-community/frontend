fffbc1d49cd0fc80ac0ebe8ad80222617ab0a3c3
jiaezzang <visveryver2@gmail.com>
2024-12-17 09:35:54 +0900

    ✨ feat : 아티클 페이지 및 컴포넌트 생성

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,6 +6,7 @@ import NotFound from '@/pages/NotFound';
 import Projects from '@/pages/Projects';
 import SignIn from '@/pages/SignIn';
 
+import Article from './pages/Article';
 import Swagger from './pages/Swagger';
 
 export default function Router() {
@@ -15,6 +16,7 @@ export default function Router() {
       <Route path='/' element={<App />}>
         <Route path='/' element={<Main />} />
         <Route path='/projects' element={<Projects />} />
+        <Route path='/article' element={<Article />} />
       </Route>
       <Route path='/api' element={<Swagger />} />
       <Route path='*' element={<NotFound />} />
---
+++ src/pages/Article/index.tsx
@@ -0,0 +1,18 @@
+import Content from '@/components/Content';
+
+import Post from './components/Post';
+import { S } from './style';
+
+/**
+ * 아티클 포스트 페이지
+ */
+export default function Article() {
+  return (
+    <Content>
+      <S.Container>
+        {/* TODO(지애): 포스트 데이터 받아와서 렌더링 */}
+        <Post />
+      </S.Container>
+    </Content>
+  );
+}
