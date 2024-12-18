70e5b1bf2dbf20c71a6348728db919bfc80bed68
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-12-18 17:45:37 +0900

    refactor: 스타일 파일 분리

--- src/components/ProjectSideContainer/index.tsx
+++ src/components/ProjectSideContainer/index.tsx
@@ -1,21 +1,7 @@
 import { PropsWithChildren } from 'react';
 
-import styled from '@emotion/styled';
+import { S } from '@/components/ProjectSideContainer/style';
 
 export default function ProjectSideContainer({ children }: PropsWithChildren) {
   return <S.SideContainer>{children}</S.SideContainer>;
 }
-
-const S = {
-  SideContainer: styled.div({
-    display: 'flex',
-    flexDirection: 'column',
-
-    height: '100%',
-
-    padding: '4rem 1.6rem',
-    gap: '1.6rem',
-
-    userSelect: 'none',
-  }),
-};
---
+++ src/components/ProjectSideContainer/style/index.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const S = {
+  SideContainer: styled.div({
+    display: 'flex',
+    flexDirection: 'column',
+
+    height: '100%',
+
+    padding: '4rem 1.6rem',
+    gap: '1.6rem',
+
+    userSelect: 'none',
+  }),
+};
