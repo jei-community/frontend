1ef6cc19fa69b09b5c60b06d420bfa3b42a11119
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-12-18 17:05:23 +0900

    refactor: Divider default props 정의

--- src/components/Divider/index.tsx
+++ src/components/Divider/index.tsx
@@ -3,9 +3,9 @@ import { S } from '@/components/Divider/style';
 export type DividerDirection = 'vertical' | 'horizontal';
 
 interface Props {
-  direction: DividerDirection;
+  direction?: DividerDirection;
 }
 
-export default function Divider({ direction }: Props) {
+export default function Divider({ direction = 'horizontal' }: Props) {
   return <S.Divider $direction={direction} />;
 }
