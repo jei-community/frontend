95356b95a4920f82a5ca41fb0e7c053d65653b3d
임범규 <202380111@jei.com>
2024-12-18 17:13:52 +0900

    🐛 fix: Ref의 타입에 null 추가

--- src/components/DraggableScroll/index.tsx
+++ src/components/DraggableScroll/index.tsx
@@ -4,7 +4,7 @@ import useDraggable from './hooks/useDraggable';
 import { S } from './style';
 
 export default function DraggableScroller({ children }: PropsWithChildren) {
-  const containerRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement | null>(null);
   const events = useDraggable(containerRef);
 
   return (
