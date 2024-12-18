1d8f858fcb4ce3c5b972e050a85e76a3d6546f77
임범규 <202380111@jei.com>
2024-12-18 17:16:43 +0900

    ♻️ refactor: 텍스트가 지정한 줄 수보다 넘치는지 확인하는 로직 훅으로 분리

--- src/pages/Main/components/Article/ArticleItem.tsx
+++ src/pages/Main/components/Article/ArticleItem.tsx
@@ -1,22 +1,10 @@
-import { useEffect, useRef, useState } from 'react';
-
 import { Thumbnail } from '../../assets';
+import useClampCheck from '../../hooks/useClampCheck';
 import ReadMoreButton from '../ReadMoreButton';
 import { S } from './style';
 
 export default function ArticleItem() {
-  const [isClamped, setIsClamped] = useState(false);
-  const contentRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (contentRef.current) {
-      const lineHeight = parseFloat(getComputedStyle(contentRef.current).lineHeight); // 한 줄의 높이
-      const maxHeight = lineHeight * 3; // 텍스트 세줄의 높이
-      const actualHeight = contentRef.current.offsetHeight; // 실제 텍스트 높이
-
-      setIsClamped(actualHeight > maxHeight); // 콘텐츠 높이가 3줄 초과인지 확인
-    }
-  }, []);
+  const { isClamped, elementRef } = useClampCheck<HTMLDivElement>(3); // 3줄 초과 체크
 
   return (
     <S.Container>
@@ -28,7 +16,7 @@ export default function ArticleItem() {
         <S.Time>5시간 전</S.Time>
       </S.ArticleMetaWrapper>
       <S.Description.title>React version 19 has been released! React version 19 has been released!</S.Description.title>
-      <S.Description.content ref={contentRef} $isClamped={isClamped}>
+      <S.Description.content ref={elementRef} $isClamped={isClamped}>
         Lorem ipsum dolor sit amet consectetur. In convallis mi mauris euismod risus vitae gravida libero. Suspendisse nec in amet at malesuada.
         Vestibulum hac vulputate praesent venenatis facilisi molestie egestas placerat. Lorem ipsum dolor sit amet consectetur. In convallis mi mauris
         euismod risus vitae gravida libero. Suspendisse nec in amet at malesuada. Vestibulum hac vulputate praesent venenatis facilisi molestie
