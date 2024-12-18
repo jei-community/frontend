9154f7022db927b402ba14d35ac3ff9f4813a200
sckroll <2023a0014@jei.com>
2024-12-18 15:53:22 +0900

    ✨ feat: 공통 텍스트 필드 컴포넌트 구현 및 스토리 추가

---
+++ src/components/TextField/style/index.ts
@@ -0,0 +1,71 @@
+import { Theme } from '@emotion/react';
+import styled from '@emotion/styled';
+
+import { TextFieldHeightSize } from '../types';
+
+interface TextFieldStyleProps {
+  $heightSize: TextFieldHeightSize;
+  $isError: boolean;
+}
+
+/** 텍스트 필드의 높이 사이즈 프로퍼티에 따른 리터럴 값 */
+const HEIGHT: { [key in TextFieldHeightSize]: string } = {
+  small: '3.2rem',
+  medium: '4rem',
+  large: '4.4rem',
+};
+
+const getFontSize = (theme: Theme, heightSize: TextFieldHeightSize) => {
+  if (heightSize === 'small') return theme.typography.body3;
+  else if (heightSize === 'medium') return theme.typography.body1;
+  else return theme.typography.subtitle;
+};
+
+export const S = {
+  /** 공통 텍스트 필드 컴포넌트 스타일 */
+  TextField: styled.input<TextFieldStyleProps>`
+    width: '100%';
+    height: ${({ $heightSize }) => HEIGHT[$heightSize]};
+    padding: 0.8rem 1.2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 0.4rem;
+    outline: none;
+    transition: all 0.15s;
+
+    // 폰트 스타일
+    ${({ theme, $heightSize }) => getFontSize(theme, $heightSize)}
+
+    // 배경 및 테두리 스타일
+    background-color: ${({ theme }) => theme.colors.white};
+    border: 2px solid ${({ theme, $isError }) => ($isError ? theme.colors.error[500] : theme.colors.gray[300])};
+
+    // 텍스트 필드 상태에 따른 스타일
+    &::placeholder {
+      color: ${({ theme }) => theme.colors.gray[400]};
+    }
+    &:hover {
+      border: 2px solid ${({ theme, $isError }) => ($isError ? theme.colors.error[500] : theme.colors.gray[500])};
+    }
+    &:active,
+    &:focus {
+      border: 2px solid ${({ theme, $isError }) => ($isError ? theme.colors.error[500] : theme.colors.primary[500])};
+    }
+    &:focus:hover {
+      border: 2px solid ${({ theme, $isError }) => ($isError ? theme.colors.error[500] : theme.colors.primary[500])};
+    }
+    &:disabled {
+      background-color: ${({ theme }) => theme.colors.gray[100]};
+      border: 2px solid ${({ theme }) => theme.colors.gray[200]};
+      color: ${({ theme }) => theme.colors.gray[200]};
+      cursor: not-allowed;
+    }
+    &:disabled:hover {
+      border: 2px solid ${({ theme }) => theme.colors.gray[200]};
+    }
+    &:disabled::placeholder {
+      color: ${({ theme }) => theme.colors.gray[200]};
+    }
+  `,
+};
