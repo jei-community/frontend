5ef5cb4f2a602e451f3b14f3539b25fcb22d1d21
임범규 <202380111@jei.com>
2024-12-20 08:48:01 +0900

    ✨ feat: 마크다운 에디터 기능 추가

---
+++ src/pages/Post/components/PostingLink/index.tsx
@@ -0,0 +1,10 @@
+import { S } from './style';
+
+/** 클릭시 포스트 작성페이지로 이동하는 버튼 */
+export default function PostingLink() {
+  return (
+    <S.EditLink to='/posts/editor'>
+      <S.EditText>글쓰기</S.EditText>
+    </S.EditLink>
+  );
+}
