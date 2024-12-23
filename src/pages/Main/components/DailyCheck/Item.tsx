318aeb8ef464236e7aa0a8dbe81f4a9e93f432a6
jiaezzang <visveryver2@gmail.com>
2024-12-23 17:05:03 +0900

    ✨ feat : 메인 우측 사이드 컴포넌트 일일점검 생성성

--- src/pages/Main/components/DailyCheck/Item.tsx
+++ src/pages/Main/components/DailyCheck/Item.tsx
@@ -1,11 +1,48 @@
+import { useState } from 'react';
+
+import { postCellForUser } from '@/apis/dailyCheck';
+import { SHEET_CONFIG } from '@/apis/dailyCheck/constant';
+import { SheetKey, TodayCheckResponse } from '@/apis/dailyCheck/type';
+
+import Button from '@/components/Button';
+import HtmlParser from '@/components/HtmlParser';
+
 import { S } from './style';
 
+interface Props {
+  /** 일일점검 프로젝트 정보 */ item: TodayCheckResponse;
+  /** 화면에 그려지는 데이터를 업데이트하는 함수 */ handleUpdateData: (title: string, col: string, value: string) => void;
+}
+
+//TODO(지애) : 임시 데이터
+const userName = '임범규';
+
 /** 개별 일일점검 아이템 (프로젝트 단위) */
-export default function Item() {
+export default function Item({ item, handleUpdateData }: Props) {
+  /** 오늘 날짜(일) */
+  const today = new Date().getDate().toString();
+  const [isChecked, setIsChecked] = useState(item.data[today] === 'O');
+
+  /** 체크 버튼을 클릭하면 동작한다. */
+  const handleCheckBtnClick = () => {
+    const value = isChecked ? '' : 'O';
+    const sheetKey = Object.keys(SHEET_CONFIG).find((el) => SHEET_CONFIG[el as SheetKey].id === item.id) as SheetKey;
+
+    if (!sheetKey) return;
+    handleUpdateData(item.title, today, value);
+    postCellForUser(sheetKey, userName, today, value);
+    setIsChecked((prev) => !prev);
+  };
+
   return (
     <S.Item.Container>
-      <S.Item.Title></S.Item.Title>
-      <S.Item.Description></S.Item.Description>
+      <S.Item.Title>{item.title}</S.Item.Title>
+      <S.Item.Description>
+        <HtmlParser htmlString={item.data['구분']} />
+      </S.Item.Description>
+      <Button color={isChecked ? 'neutral' : 'primary'} size='full' onClick={handleCheckBtnClick}>
+        {isChecked ? '🎉 점검 완료!' : '✅ 점검 완료하기'}
+      </Button>
     </S.Item.Container>
   );
 }
--- src/pages/Main/components/DailyCheck/index.tsx
+++ src/pages/Main/components/DailyCheck/index.tsx
@@ -1,14 +1,24 @@
-import Item from './Item';
+import { Suspense } from 'react';
+
+import { getTodayCheck } from '@/apis/dailyCheck';
+
+import ItemsWrapper from './ItemsWrapper';
 import { S } from './style';
+
+//TODO(지애) : 임시 데이터 수정
+const userName = '임범규';
 
 /** 메인페이지 내 일일점검 컴포넌트 */
 export default function DailyCheck() {
+  /** 로그인한 유저의 당일 일일점검 데이터 요청 */
+  const TodayCheckPromise = getTodayCheck(userName);
+
   return (
     <S.Container>
       <S.Title>일일점검</S.Title>
-      <S.ItemsWrapper>
-        <Item />
-      </S.ItemsWrapper>
+      <Suspense fallback={<div>'...로딩중이라고'</div>}>
+        <ItemsWrapper todayCheckPromise={TodayCheckPromise} />
+      </Suspense>
     </S.Container>
   );
 }
