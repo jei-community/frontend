e338d48a5b784f4a06167d48ee764b22c79746d5
임범규 <202380111@jei.com>
2024-12-24 08:37:10 +0900

    💄 style: 메인페이지 UI 수정

--- src/pages/Main/components/Post/style/index.ts
+++ src/pages/Main/components/Post/style/index.ts
@@ -4,6 +4,14 @@ import MDEditor from '@uiw/react-md-editor';
 export const S = {
   Container: styled.div({
     width: '100%',
+    boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
+    padding: '2.4rem',
+    borderRadius: '0.8rem',
+    transition: 'transform 0.2s ease, box-shadow 0.2s ease',
+    ':hover': {
+      transform: 'translateY(-4px)',
+      boxShadow: '0px 6px 8px rgba(0, 0, 0, 0.2)',
+    },
   }),
 
   Title: styled.div(({ theme }) => ({
@@ -45,45 +53,26 @@ export const S = {
     marginBottom: '1.6rem',
   }),
 
-  Description: styled.div(({ theme }) => ({
-    ...theme.typography.body1,
-    color: theme.colors.gray[800],
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    display: '-webkit-box',
-    lineClamp: 5,
-    WebkitLineClamp: 5,
-    WebkitBoxOrient: 'vertical',
-  })),
-
   Markdown: styled(MDEditor.Markdown)({
     backgroundColor: '#ffffff',
     color: '#000000',
     padding: '1.6rem',
     borderRadius: '0.8rem',
+    width: '100%',
     maxHeight: '16rem',
     overflow: 'hidden',
   }),
 
-  MarkdownWrapper: styled.button(({ theme }) => ({
+  MarkdownWrapper: styled.div(({ theme }) => ({
     position: 'relative',
     backgroundColor: '#ffffff',
     color: '#000000',
-    border: '1px solid',
-    borderColor: theme.colors.gray[100],
-    borderRadius: '0.8rem',
     maxHeight: '16rem',
     overflow: 'hidden',
     cursor: 'pointer',
-    '&:after': {
-      // eslint-disable-next-line quotes
-      content: "''",
-      position: 'absolute',
-      bottom: 0,
-      left: 0,
-      right: 0,
-      height: '10rem',
-      background: 'linear-gradient(rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.8) 100%)',
-    },
+    width: '100%',
+    border: '1px solid',
+    borderColor: theme.colors.gray[100],
+    borderRadius: '0.8rem',
   })),
 };
