8527fab730d112569d79e5a033c47c744e1eb581
sckroll <2023a0014@jei.com>
2024-12-23 10:58:23 +0900

    ✨ feat: 빈 내용 영역 컴포넌트 구현 및 스토리 추가

---
+++ src/components/EmptyContent/stories/EmptyContent.stories.ts
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import EmptyContent from '..';
+
+const meta = {
+  component: EmptyContent,
+  tags: ['autodocs', '!dev'],
+  args: { size: 'small', children: '담당 중인 일일점검이 없어요' },
+} satisfies Meta<typeof EmptyContent>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const SmallEmptyContent: Story = {
+  args: {
+    size: 'small',
+    children: '담당 중인 일일점검이 없어요',
+  },
+};
+
+export const MediumEmptyContent: Story = {
+  args: {
+    size: 'medium',
+    children: '참여 중이거나 참여한 프로젝트가 없어요',
+  },
+};
+
+export const LargeEmptyContent: Story = {
+  args: {
+    size: 'large',
+    children: '볼 수 있는 앨범이 없어요',
+  },
+};
