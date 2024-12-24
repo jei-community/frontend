b1626e3e2a7a09ebd262a4fe60f948d01d517870
jiaezzang <visveryver2@gmail.com>
2024-12-24 12:16:01 +0900

    ✨ feat : 구글로그인 요청 api

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,3 +1,5 @@
+import { getGoogleLogin } from '@/apis/auth';
+
 import GlobalStyle from '@/styles/GlobalStyle';
 import Logo from '@/styles/assets/images/logo_white.svg';
 
@@ -8,13 +10,15 @@ import { S } from './style';
  * 로그인 페이지
  */
 export default function SignIn() {
+  const GoogleLogin = async () => getGoogleLogin();
+
   return (
     <GlobalStyle>
       <S.WhiteDim />
       <S.Container>
         <S.SignInContainer>
           <S.Logo src={Logo} alt='EVERYDEI' />
-          <S.GoogleLogin.Container>
+          <S.GoogleLogin.Container onClick={GoogleLogin}>
             <S.GoogleLogin.Logo src={GoogleLogo} alt='Google' />
             <S.GoogleLogin.Text>Google 계정으로 로그인</S.GoogleLogin.Text>
           </S.GoogleLogin.Container>
