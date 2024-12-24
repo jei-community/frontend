ff3f91e9cd89adede9b09f3464674e7e19294bb9
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-12-24 14:50:59 +0900

    feat: CenteredWrapper 컴포넌트 구현

---
+++ src/components/CenteredWrapper/style/index.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const S = {
+  Wrapper: styled.div({
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  }),
+};
