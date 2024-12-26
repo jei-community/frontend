781242cafbc8d43424b3273463f340f658b1224c
임범규 <202380111@jei.com>
2024-12-26 11:39:25 +0900

    ✨ feat: 왼쪽 사이드에 들어갈 프로필 정보 컴포넌트 구현

--- src/pages/AlbumList/index.tsx
+++ src/pages/AlbumList/index.tsx
@@ -1,11 +1,18 @@
 import Aside from '@/components/Aside';
 import Content from '@/components/Content';
+import Profile from '@/components/Profile';
+
+import { S } from './style';
 
 export default function AlbumList() {
   return (
     <>
-      <Aside />
-      <Content>1234</Content>
+      <Aside>
+        <Profile />
+      </Aside>
+      <Content>
+        <S.ContentContainer>1234</S.ContentContainer>
+      </Content>
       <Aside />
     </>
   );
