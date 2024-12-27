13f250bc0ddbfafbe55128049da9bb1132ca0720
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-12-27 10:40:03 +0900

    refactor: 공통 컴포넌트로 교체

--- src/pages/ProjectList/components/ProjectListHeader/index.tsx
+++ src/pages/ProjectList/components/ProjectListHeader/index.tsx
@@ -1,6 +1,8 @@
 import { ProjectListData } from '@/types/project';
 
-import ProjectListToggle from '@/pages/ProjectList/components/ProjectListToggle';
+import ButtonGroup from '@/components/ButtonGroup';
+import { ButtonGroupItem } from '@/components/ButtonGroup/types';
+
 import Searchbar from '@/pages/ProjectList/components/Searchbar';
 import { useIsMyProjectSelected } from '@/pages/ProjectList/hooks';
 import { S } from '@/pages/ProjectList/style';
@@ -14,6 +16,10 @@ interface Props {
 
 export default function ProjectListHeader({ projectList, updateProjectList, selectMyProject, selectProjectAll }: Props) {
   const { isMyProjectSelected, updateIsMyProjectSelected } = useIsMyProjectSelected(false);
+  const items: ButtonGroupItem[] = [
+    { label: '', value: '전체', color: 'success', checked: !isMyProjectSelected },
+    { label: '', value: '마이', color: 'warning', checked: isMyProjectSelected },
+  ];
 
   const showSelectedProjects = (isMyProjectSelected: boolean) => {
     if (isMyProjectSelected) selectMyProject();
@@ -37,8 +43,10 @@ export default function ProjectListHeader({ projectList, updateProjectList, sele
 
   return (
     <S.HeaderContainer>
-      <ProjectListToggle isMyProjectSelected={isMyProjectSelected} toggleIsMyProjectSelected={toggleIsMyProjectSelected} />
-
+      <S.ButtonContainer>
+        <ButtonGroup items={items} onChange={toggleIsMyProjectSelected} />
+        <S.MyProjectLabelText>{isMyProjectSelected ? '전체 프로젝트 보기' : '내 프로젝트만 보기'}</S.MyProjectLabelText>
+      </S.ButtonContainer>
       <Searchbar isMyProjectSelected={isMyProjectSelected} showSelectedProjects={showSelectedProjects} filterProjects={filterProjects} />
     </S.HeaderContainer>
   );
