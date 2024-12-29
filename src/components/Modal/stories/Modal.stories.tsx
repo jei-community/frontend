7503a01046e0bd0f0d08a3dd64700b592eb5471d
Sckroll <kimsc0714@gmail.com>
2024-12-29 23:50:09 +0900

    ✅ update: 모달 스토리 인자에 `id` 프로퍼티 추가

--- src/components/Modal/stories/Modal.stories.tsx
+++ src/components/Modal/stories/Modal.stories.tsx
@@ -12,13 +12,13 @@ import { ModalProps } from '../types';
  *
  * (Zustand 스토어를 사용하는 컴포넌트를 만들어서 테스트)
  */
-function ModalWithHooks({ title, subtitle, onConfirm, onCancel, children }: ModalProps) {
+function ModalWithHooks({ id, title, subtitle, onConfirm, onCancel, children }: ModalProps) {
   const { onOpenModal } = useModalStore();
 
   return (
     <>
-      <Button onClick={() => onOpenModal('test')}>Open Modal</Button>
-      <Modal id='test' title={title} subtitle={subtitle} onConfirm={onConfirm} onCancel={onCancel}>
+      <Button onClick={() => onOpenModal(id)}>Open Modal</Button>
+      <Modal id={id} title={title} subtitle={subtitle} onConfirm={onConfirm} onCancel={onCancel}>
         {children}
       </Modal>
     </>
@@ -29,7 +29,7 @@ const meta = {
   title: 'Modal',
   component: ModalWithHooks,
   tags: ['autodocs', '!dev'],
-  args: { title: 'Title', subtitle: 'Subtitle', onConfirm: fn(), onCancel: fn(), children: 'Content' },
+  args: { id: 'test', title: 'Title', subtitle: 'Subtitle', onConfirm: fn(), onCancel: fn(), children: 'Content' },
 } satisfies Meta<typeof ModalWithHooks>;
 
 export default meta;
@@ -37,5 +37,5 @@ export default meta;
 type Story = StoryObj<typeof ModalWithHooks>;
 
 export const DefaultModal: Story = {
-  args: { title: 'Title', subtitle: 'Subtitle', children: 'Content' },
+  args: { id: 'test', title: 'Title', subtitle: 'Subtitle', children: 'Content' },
 };
