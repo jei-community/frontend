90962bc5e5ec42d7f397af60a5f08a6d83623d5a
sckroll <2023a0014@jei.com>
2024-12-30 21:07:54 +0900

    ♻️ refactor: 모달 및 토스트 스토리 리팩토링

--- src/components/Toast/stories/Toast.stories.tsx
+++ src/components/Toast/stories/Toast.stories.tsx
@@ -27,19 +27,21 @@ function ToastWithHooks({ id, duration, color, onCancel, children }: ToastProps)
 
 const meta = {
   title: 'Toast',
-  component: ToastWithHooks,
+  component: Toast,
   tags: ['autodocs', '!dev'],
-  args: { id: 'test', duration: 5000, color: 'primary', onCancel: fn(), children: '안녕하세요! 이건 토스트에요.' },
-} satisfies Meta<typeof ToastWithHooks>;
+  args: { id: 'test', duration: 5000, color: 'primary', onCancel: fn() },
+} satisfies Meta<typeof Toast>;
 
 export default meta;
 
-type Story = StoryObj<typeof ToastWithHooks>;
+type Story = StoryObj<typeof Toast>;
 
 export const DefaultToast: Story = {
-  args: { id: 'test', duration: 5000, color: 'primary', onCancel: fn(), children: '안녕하세요! 이건 토스트에요.' },
+  args: { id: 'test', duration: 5000, color: 'primary', onCancel: fn() },
+  render: (arg) => <ToastWithHooks {...arg}>안녕하세요! 이건 토스트에요.</ToastWithHooks>,
 };
 
 export const ShortToast: Story = {
-  args: { id: 'short', duration: 3000, color: 'error', onCancel: fn(), children: '3초만 나타나는 토스트입니다.' },
+  args: { id: 'short', duration: 3000, color: 'error', onCancel: fn() },
+  render: (arg) => <ToastWithHooks {...arg}>3초만 나타나는 토스트입니다.</ToastWithHooks>,
 };
