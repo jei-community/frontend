346c9602b359c95d8f3e58e9d626670e410903b4
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-12-31 13:55:10 +0900

    refactor: 상수로 리팩터링

--- src/pages/ProjectEditor/hooks/useDeleteProjectMutation.ts
+++ src/pages/ProjectEditor/hooks/useDeleteProjectMutation.ts
@@ -2,6 +2,8 @@ import { deleteProject } from 'everydei-api-dev/lib/apis/functional/projects';
 
 import { getNestiaHeader } from '@/utils/api';
 
+import { QUERY_KEYS } from '@/constants/query';
+
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export const useDeleteProjectMutation = () => {
@@ -13,7 +15,7 @@ export const useDeleteProjectMutation = () => {
       return deleteProject(getNestiaHeader(), projectId);
     },
 
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['projectList'] }),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.PROJECT_LIST] }),
     onError: () => alert('프로젝트 삭제에 실패했습니다'),
   });
 };
