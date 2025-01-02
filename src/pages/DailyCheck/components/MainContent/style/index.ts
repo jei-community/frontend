de272219aea1bb5dd0c62e6c3c95365cb8ae298c
jiaezzang <visveryver2@gmail.com>
2025-01-02 12:33:15 +0900

    ✅ update : 빈 컨텐츠 공통 컴포넌트로 교체

---
+++ src/pages/DailyCheck/components/MainContent/style/index.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+/** 일일점검 메인 컨텐츠 컴포넌트 스타일 */
+export const S = {
+  /** 빈 컨텐츠일 때 영역 */
+  EmptyContentContainer: styled.div({
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+    height: '100vh',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }),
+};
